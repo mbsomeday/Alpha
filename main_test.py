2539cdc5a2134f24ed88083b3a793c28742147cb
mbsomeday <137542994@qq.com>
2025-06-26 13:38:31 +0100

    update

--- main_test.py
+++ main_test.py
@@ -6,32 +6,31 @@ sys.path.append(root_path)
 
 import argparse
 
-# from training.training_template import Ped_Classifier
+from training.training_template import Ped_Classifier
 from configs.pedCls_args import BaseArgs, TrainArgs, TestArgs
+
+
+opts = TestArgs().parse()
+ped_cls = Ped_Classifier(opts=opts)
+ped_cls.test()
+
+
+# from test_func.ds_cls import test_ds_classifier
+# def get_opts():
+#     parser = argparse.ArgumentParser()
+#     """Define the common options that are used in both training and test."""
+#     parser.add_argument('--weights_path', type=str)
+#     return parser.parse_args()
 #
-#
-# opts = TestArgs().parse()
-# ped_cls = Ped_Classifier(opts=opts)
-# ped_cls.test()
-
-from test_func.ds_cls import test_ds_classifier
-
-
-def get_opts():
-    parser = argparse.ArgumentParser()
-    """Define the common options that are used in both training and test."""
-    parser.add_argument('--weights_path', type=str)
-    return parser.parse_args()
-
-opts = get_opts()
-weights_path = opts.weights_path
-ds_cls = test_ds_classifier(model_obj='models.EfficientNet.efficientNetB0',
-                            ds_name_list=['D1', 'D2'],
-                            batch_size=96,
-                            weights_path=weights_path,
-                            txt_name='test.txt',
-                            path_key='Stage6_org'
-                            )
+# opts = get_opts()
+# weights_path = opts.weights_path
+# ds_cls = test_ds_classifier(model_obj='models.EfficientNet.efficientNetB0',
+#                             ds_name_list=['D1', 'D2'],
+#                             batch_size=96,
+#                             weights_path=weights_path,
+#                             txt_name='test.txt',
+#                             path_key='Stage6_org'
+#                             )
 
 
 

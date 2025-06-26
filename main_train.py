96064d290ebcd3dd37bd3033420ec34b7da3f19c
mbsomeday <137542994@qq.com>
2025-06-26 11:50:56 +0100

    update

--- main_train.py
+++ main_train.py
@@ -6,24 +6,25 @@ sys.path.append(root_path)
 
 import argparse
 
-# from training.training_template import Ped_Classifier
-# from configs.pedCls_args import TrainArgs
-from training.training import train_ds_model_alpha
+from training.training_template import Ped_Classifier
+from configs.pedCls_args import TrainArgs
 
 
-# opts = TrainArgs().parse()
-# ped_cls = Ped_Classifier(opts=opts)
-# ped_cls.train()
+opts = TrainArgs().parse()
+ped_cls = Ped_Classifier(opts=opts)
+ped_cls.train()
 
-model_obj = 'models.EfficientNet.efficientNetB0'
-ds_cls = train_ds_model_alpha(model_obj=model_obj,
-                              batch_size=64,
-                              path_key='Stage6_org',
-                              ds_name_list=['D1', 'D2'],
-                              warmup_epochs=3,
-                              lr_patience=5
-                              )
-ds_cls.train_model()
+
+# from training.training import train_ds_model_alpha
+# model_obj = 'models.EfficientNet.efficientNetB0'
+# ds_cls = train_ds_model_alpha(model_obj=model_obj,
+#                               batch_size=64,
+#                               path_key='Stage6_org',
+#                               ds_name_list=['D1', 'D2'],
+#                               warmup_epochs=3,
+#                               lr_patience=5
+#                               )
+# ds_cls.train_model()
 
 
 

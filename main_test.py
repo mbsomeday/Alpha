da1729c6efe7d4bd2f98bc5394576d3eb91b45f3
mbsomeday <137542994@qq.com>
2025-07-02 10:07:20 +0100

    update

--- main_test.py
+++ main_test.py
@@ -10,17 +10,17 @@ from training.training_template import Ped_Classifier
 from configs.pedCls_args import BaseArgs, TrainArgs, TestArgs
 
 
-opts = TestArgs().parse()
-ped_cls = Ped_Classifier(opts=opts)
-ped_cls.test()
+# opts = TestArgs().parse()
+# ped_cls = Ped_Classifier(opts=opts)
+# ped_cls.test()
 
 
 # from test_func.ds_cls import test_ds_classifier
-# def get_opts():
-#     parser = argparse.ArgumentParser()
-#     """Define the common options that are used in both training and test."""
-#     parser.add_argument('--weights_path', type=str)
-#     return parser.parse_args()
+def get_opts():
+    parser = argparse.ArgumentParser()
+    """Define the common options that are used in both training and test."""
+    parser.add_argument('--weights_path', type=str)
+    return parser.parse_args()
 #
 # opts = get_opts()
 # weights_path = opts.weights_path
@@ -33,7 +33,16 @@ ped_cls.test()
 #                             )
 
 
+from test_func.ds_cls import test_ds_classifier
 
+
+test_ds_classifier(model_obj='models.EfficientNet.efficientNetB0',
+                   ds_name_list=['D4'],
+                   path_key='Stage6_org',
+                   weights_path=r'/kaggle/input/stage5-weights-effidscls/efficientNetB0_dsCls-10-0.97636.pth',
+                   batch_size=96,
+                   txt_name='test.txt'
+                   )
 
 
 

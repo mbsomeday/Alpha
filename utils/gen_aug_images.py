21ffaa041135125373bce08c3ac0311ebdd6c644
mbsomeday <137542994@qq.com>
2025-07-03 13:15:17 +0100

    update

--- utils/gen_aug_images.py
+++ utils/gen_aug_images.py
@@ -1,11 +1,72 @@
 from torch.utils.data import DataLoader
 from torchvision import transforms
 import torchvision.transforms.functional as F
+import matplotlib.pyplot as plt
+import os, argparse
+from tqdm import tqdm
 
 from data.dataset import my_dataset
-from utils.utils import save_image_tensor
+from utils import save_image_tensor
 
 
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--base_dir', type=str)
+
+    args = parser.parse_args()
+    return args
+
+
+args = get_args()
+
+base_dir = args.base_dir
+
+# base_dir = r'D:\my_phd\dataset\Stage6\stage6_bdd100k'
+
+get_dataset = my_dataset(ds_name_list=['D4'], path_key='Stage6_org', txt_name='test.txt')
+get_loader = DataLoader(get_dataset, batch_size=1, shuffle=False)
+
+plt_transform = transforms.ToPILImage()
+bright_transform = transforms.transforms.ColorJitter(brightness=0.2)
+
+for idx, data_dict in enumerate(tqdm(get_loader)):
+    print(data_dict.keys())
+
+    images = data_dict['image']
+    image_paths = data_dict['img_path']
+    path_contenst = image_paths[0].split(os.sep)
+    image_name = path_contenst[-1]
+    print(image_paths, path_contenst)
+
+    img_flip = F.hflip(images)
+    img_bright = bright_transform(images)
+
+    flip_name = image_name.split('.')[0] + '_flip.jpg'
+    img_flip_save_path = os.path.join(base_dir, 'augmentation_train', path_contenst[-2], flip_name)
+    save_image_tensor(img_flip, img_flip_save_path)
+
+    # print(img_flip_save_path)
+
+    bright_name = image_name.split('.')[0] + '_bright.jpg'
+    img_bright_save_path = os.path.join('augmentation_train', path_contenst[-2], bright_name)
+    save_image_tensor(bright_name, img_bright_save_path)
+
+
+
+    # plt.figure()
+    # plt.subplot(131)
+    # plt.title('org')
+    # plt.imshow(plt_transform(images[0]))
+    # plt.subplot(132)
+    # plt.title('flip')
+    # plt.imshow(plt_transform(img_flip[0]))
+    # plt.subplot(133)
+    # plt.title('brightness')
+    # plt.imshow(plt_transform(img_bright[0]))
+    # plt.show()
+
+    # break
 
 
 

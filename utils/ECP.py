be40daefccb5ec39a1a9bea8143b8532a9a10aae
mbsomeday <137542994@qq.com>
2025-07-08 22:01:58 +0100

    update

--- utils/ECP.py
+++ utils/ECP.py
@@ -60,12 +60,14 @@ def crop_pedestrian(image_path, crop_box, crop_image_name):
 def read_json(json_list):
     ped_num = 0
     # cropped_num = 0
-    # noPed_num = 0
+    noPed_num = 0
     # no_ped_list = []
-    for json_path in tqdm(json_list):
+    # ped_identity = ['pedestrian', 'buggy-group', 'person-group-far-away', 'rider']
+
+    for idx, json_path in enumerate(tqdm(json_list)):
+        no_ped_flag = True
         # stop_flag = False
         bbox_list = []
-
         path_contents = json_path.split('\\')
         image_path = os.path.join(image_dir, path_contents[-2], path_contents[-1].split('.')[0]+'.png')
         with open(json_path, 'r') as f:
@@ -74,11 +76,22 @@ def read_json(json_list):
         '''
             获取无行人的图片
         '''
-        # if len(objectives) == 0:
-        #     noPed_num += 1
+        # for obj in objectives:
+            # idt = obj['identity']
+            # if idt in ped_identity:
+            #     no_ped_flag = False
+            #     break
+        # if no_ped_flag:
         #     image_path = json_path.replace('labels', 'img')
         #     image_path = image_path.replace('json', 'png')
-        #     no_ped_list.append(image_path + '\n')
+            # no_ped_list.append(image_path + '\n')
+            # noPed_num += 1
+
+        # if len(objectives) == 0:
+        #     noPed_num += 1
+            # image_path = json_path.replace('labels', 'img')
+            # image_path = image_path.replace('json', 'png')
+            # no_ped_list.append(image_path + '\n')
         '''
             裁剪pedestrian crop
         '''
@@ -91,33 +104,25 @@ def read_json(json_list):
             tags = obj['tags']
             if obj_cls == 'pedestrian' and w > min_size and h > min_size and w < max_size and h < max_size and len(tags) == 0:
                 bbox_list.append(crop_box)
-
         bbox_list = filter_bboxes_by_iou(bbox_list)
-        # ped_num += len(bbox_list)
-
+        ped_num += len(bbox_list)
         for crop_idx, crop_box in enumerate(bbox_list):
             crop_image_name = path_contents[-1].split('.')[0] + '_' + str(crop_idx) + '.jpg'
             crop_pedestrian(image_path, crop_box, crop_image_name)
 
+        if ped_num >= 2500:
+            break
 
-                # stop_flag = True
-                # cropped_num += 1
-            # if cropped_num == 4000:
-            #     stop_flag = True
-
-        # if stop_flag:
-        #     break
-
-    # with open(no_ped_path, 'a') as f:
+    # print(f'行人数量:{ped_num}')
+    # print(f'没有行人的图片：{noPed_num}')
+    # with open(no_people_txt, 'a') as f:
     #     for item in no_ped_list:
     #         f.write(item)
 
-    # print(f'符合条件的行人：{ped_num}')
-    # print(f'没有行人的：{noPed_num}')
 
-def crop_night_no_people(no_people_txt):
+def crop_no_people(no_people_txt):
     '''
-        从night图片中裁剪no people，由于数量有限，每张图片裁剪5个，选取的图片中不包含任何标注
+        从night图片中裁剪no people，由于数量有限，每张图片裁剪n个，选取的图片中不包含任何标注
     '''
 
     def save_crop(item, image, x1, y1, obj_idx=None):
@@ -128,7 +133,9 @@ def crop_night_no_people(no_people_txt):
         crop_name = item.split('\\')[-1]
         if obj_idx is not None:
             crop_name = crop_name.replace('.', '_' + obj_idx + '.')
+        crop_name = crop_name.replace('png', 'jpg')
         crop_image_path = os.path.join(no_ped_crop_dir, crop_name)
+        # print(f'crop_image_path:{crop_image_path}')
         crop.save(crop_image_path)
 
         # # 展示裁剪图片的代码
@@ -141,8 +148,6 @@ def crop_night_no_people(no_people_txt):
         # plt.imshow(crop)
         # plt.show()
 
-
-
     with open(no_people_txt, 'r') as f:
         data = f.readlines()
     for idx, item in enumerate(tqdm(data)):
@@ -152,48 +157,80 @@ def crop_night_no_people(no_people_txt):
         '''
             裁剪night非行人
         '''
-        # for i in range(5):
-        #     x_start = i * 384
-        #     x1 = random.randint(x_start, x_start + 160)
+        # for i in range(3):
+        #     x_start = i * 640
+        #     x1 = random.randint(x_start, x_start + 416)
         #     y1 = random.randint(360, 596)
-        #     x2 = x1 + 224
-        #     y2 = y1 + 224
-        #     crop_box = (x1, y1, x2, y2)
-        #     crop = image.crop(crop_box)
-        #     crop_name = item.split('\\')[-1]
-        #     crop_name = crop_name.replace('.', '_'+str(i)+'.')
-        #     crop_image_path = os.path.join(no_ped_crop_dir, crop_name)
-        #     crop.save(crop_image_path)
+        #     save_crop(item, image, x1, y1, obj_idx=str(i))
 
         '''
             裁剪day非行人
         '''
-        two_crops_prob = random.random()
-        if two_crops_prob > 0.8:        # 从该图片中裁剪2个crop
-            for i in range(2):
-                x_start = 300 if i == 0 else 650
-                x_end = (650 - 244) if i == 0 else (1600 - 244)
-                x1 = random.randint(x_start, x_end)
-                y1 = random.randint(360, 596)
-                save_crop(item, image, x1, y1, obj_idx=str(i))
-
-        else:       # 从该图片中裁剪2个crop
-            x1 = random.randint(300, 1600 - 224)
-            y1 = random.randint(360, 596)
-            save_crop(item, image, x1, y1)
+        x1 = random.randint(300, 1600 - 224)
+        y1 = random.randint(360, 596)
+        save_crop(item, image, x1, y1)
 
         # break
 
 
+        # if two_crops_prob > 0.8:        # 从该图片中裁剪2个crop
+        #     for i in range(2):
+        #         x_start = 300 if i == 0 else 650
+        #         x_end = (650 - 244) if i == 0 else (1600 - 244)
+        #         x1 = random.randint(x_start, x_end)
+        #         y1 = random.randint(360, 596)
+        #         save_crop(item, image, x1, y1, obj_idx=str(i))
+        # else:       # 从该图片中裁剪2个crop
+        #     x1 = random.randint(300, 1600 - 224)
+        #     y1 = random.randint(360, 596)
+        #     save_crop(item, image, x1, y1)
+
+        # break
+
+
+def new_crop_no_pedestrian(no_people_list):
+    '''
+        从不包含people的图片中进行裁剪
+    '''
+    with open(no_people_txt, 'r') as f:
+        data = f.readlines()
+
+    print(f'num of no people samples:{len(data)}')
+
+    for idx, item in enumerate(tqdm(data)):
+        item = item.strip()
+        item_contents = item.split('\\')
+        image = Image.open(item).convert('RGB')
+
+        x1 = random.randint(0, 1376)
+        y1 = random.randint(360, 596)
+
+        x2 = x1 + 224
+        y2 = y1 + 224
+
+        crop_box = (x1, y1, x2, y2)
+        crop = image.crop(crop_box)
+        crop_image_path = os.path.join(no_ped_crop_dir, item_contents[-1])
+        crop.save(crop_image_path)
+        # print(crop_image_path)
+        # break
 
 
 if __name__ == '__main__':
     json_dir = r'E:\Dataset\ECP\day\labels\train'
-    image_dir = r'E:\Dataset\ECP\day\img\train'
-    ped_crop_save_dir = r'D:\my_phd\dataset\Stage6\stage6_ecp\ped_day'
-    # no_people_txt = r'D:\my_phd\dataset\Stage6\stage6_ecp\no_ped_day.txt'
-    # no_ped_crop_dir = r'D:\my_phd\dataset\Stage6\stage6_ecp\noPed_day'
-    #
+    base_dir = r'D:\my_phd\dataset\Stage6\stage6_ecp'
+    # cls_name = 'nonPedestrian'
+    cls_name = 'pedestrian'
+
+    # no_people_txt = os.path.join(base_dir, 'noPed_day_train.txt')
+    image_dir = json_dir.replace('labels', 'img')
+    # no_ped_crop_dir = os.path.join(base_dir, 'noPed_day_train')
+    ped_crop_save_dir = os.path.join(base_dir, 'ped_day')
+    # crop_save_dir = os.path.join(base_dir, cls_name)
+    # if not os.path.exists(crop_save_dir):
+    #     print(f'创建{crop_save_dir}')
+    #     os.mkdir(crop_save_dir)
+
     min_size = 50
     max_size = 224
     image_width = 1920
@@ -201,16 +238,16 @@ if __name__ == '__main__':
     json_list = []
 
     location_list = os.listdir(json_dir)
-
     for dir_name in tqdm(location_list):
         dir_path = os.path.join(json_dir, dir_name)
         json_list.extend(os.path.join(dir_path, j_name) for j_name in os.listdir(dir_path))
-
-    # random.shuffle(json_list)
-    print(f'num of json: {len(json_list)}')
+    random.shuffle(json_list)
+    print(f'num of json in {json_dir}: {len(json_list)}')
     read_json(json_list)
 
-    # crop_night_no_people(no_people_txt)
+    # crop_no_people(no_people_txt)
+
+
 
 
 

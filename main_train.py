35d425391d333572803dfb8f207098c1388e5341
mbsomeday <137542994@qq.com>
2025-07-12 22:39:28 +0100

    update

--- main_train.py
+++ main_train.py
@@ -7,50 +7,17 @@ sys.path.append(root_path)
 import argparse
 from training.training_template import ds_classifier
 
-'''
-    训练ds classifier
-'''
-def get_args():
-    parser = argparse.ArgumentParser()
 
-    parser.add_argument('--ds_model_obj', type=str, default='models.EfficientNet.efficientNetB0')
-    parser.add_argument('--ds_name_list', nargs='+', default=['D1', 'D2', 'D3'], help='dataset list for ds classifier')
-    parser.add_argument('--data_key', type=str, default='Stage6_org')
-    parser.add_argument('--train_batch_size', type=int, default=64)
-    parser.add_argument('--init_method', type=str, default='orthogonal', help='the way to initialize model weights, e.g., kaiming, orthogonal')
-    parser.add_argument('--base_lr', type=float, default=0.01)
-    parser.add_argument('--isTrain', action='store_true')
-    parser.add_argument('--max_epochs', type=int, default=100)
-    parser.add_argument('--warmup_epochs', type=int, default=3)
-    parser.add_argument('--val_batch_size', type=int, default=128)
-
-    # callbacks
-    parser.add_argument('--top_k', type=int, default=3)
-    parser.add_argument('--patience', type=int, default=10)
-
-    # test
-    parser.add_argument('--ds_weights_path', type=str, default=None)
-    parser.add_argument('--test_batch_size', type=int, default=128)
-    parser.add_argument('--test_txt_name', type=str, default='test.txt')
-
-    args = parser.parse_args()
-
-    return args
-
-opts = get_args()
-ds_model = ds_classifier(opts)
-# ds_model.train()
-ds_model.test()
 
 '''
 训练ped classifier
 '''
 from training.training_template import Ped_Classifier
 from configs.pedCls_args import TrainArgs
-#
-# opts = TrainArgs().parse()
-# ped_cls = Ped_Classifier(opts=opts)
-# ped_cls.train()
+
+opts = TrainArgs().parse()
+ped_cls = Ped_Classifier(opts=opts)
+ped_cls.train()
 
 
 '''

fd0830ddc7d696c273bfe3f6e9493384618f336a
mbsomeday <137542994@qq.com>
2025-07-13 11:33:37 +0100

    update

--- training/training_template.py
+++ training/training_template.py
@@ -244,7 +244,7 @@ class Ped_Classifier():
             self.train_loader = DataLoader(self.train_dataset, batch_size=self.opts.batch_size, shuffle=True)
         else:
 
-            self.train_dataset = my_dataset(ds_name_list=self.opts.ds_name_list, path_key=self.opts.data_key, txt_name='augmentation.txt')
+            self.train_dataset = my_dataset(ds_name_list=self.opts.ds_name_list, path_key=self.opts.data_key, txt_name='augmentation_train.txt')
             self.train_loader = DataLoader(self.train_dataset, batch_size=self.opts.batch_size, shuffle=True)
 
         self.val_dataset = my_dataset(ds_name_list=self.opts.ds_name_list, path_key=self.opts.data_key, txt_name='val.txt')

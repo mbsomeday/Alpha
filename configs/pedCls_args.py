c62c63da001e06439d3892b192b5202a0167149b
mbsomeday <137542994@qq.com>
2025-07-14 17:14:01 +0100

    update

--- configs/pedCls_args.py
+++ configs/pedCls_args.py
@@ -51,6 +51,7 @@ class TrainArgs(BaseArgs):
         parser.add_argument('--opt_key_path', type=str, default=None, help='operated image key path')
         parser.add_argument('--beta', type=float, default=0.0)
         parser.add_argument('--fade_images_only', action='store_true', help='if only train with fade images')
+        parser.add_argument('--operated_dir_name', type=str, default=None)
 
         # model
         parser.add_argument('--init_method', type=str, default='orthogonal', help='the way to initialize model weights, e.g., kaiming, orthogonal')

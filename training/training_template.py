9da2212d4f79b7342a235a6bee39c658a18fa4f2
mbsomeday <137542994@qq.com>
2025-07-21 13:36:37 +0100

    update

--- training/training_template.py
+++ training/training_template.py
@@ -245,14 +245,14 @@ class Ped_Classifier():
             self.train_dataset = operated_dsimages(ds_name_list=self.opts.ds_name_list, path_key=self.opts.data_key, txt_name_list=self.opts.train_txt_name_list, operated_dir_name=self.opts.operated_dir_name)
             self.train_loader = DataLoader(self.train_dataset, batch_size=self.opts.batch_size, shuffle=True)
             # val的时候用fade images
-            self.val_dataset = operated_dsimages(ds_name_list=self.opts.ds_name_list, path_key=self.opts.data_key, txt_name_list=['val.txt'], operated_dir_name='layerCAM78_val')
-            self.val_loader = DataLoader(self.val_dataset, batch_size=self.opts.batch_size, shuffle=False)
+            # self.val_dataset = operated_dsimages(ds_name_list=self.opts.ds_name_list, path_key=self.opts.data_key, txt_name_list=['val.txt'], operated_dir_name='layerCAM78_val')
+            # self.val_loader = DataLoader(self.val_dataset, batch_size=self.opts.batch_size, shuffle=False)
         else:
             self.train_dataset = my_dataset(ds_name_list=self.opts.ds_name_list, path_key=self.opts.data_key, txt_name_list=self.opts.train_txt_name_list)
             self.train_loader = DataLoader(self.train_dataset, batch_size=self.opts.batch_size, shuffle=True)
 
-        # self.val_dataset = my_dataset(ds_name_list=self.opts.ds_name_list, path_key=self.opts.data_key, txt_name_list=self.opts.val_txt_name_list)
-        # self.val_loader = DataLoader(self.val_dataset, batch_size=self.opts.batch_size, shuffle=False)
+        self.val_dataset = my_dataset(ds_name_list=self.opts.ds_name_list, path_key=self.opts.data_key, txt_name_list=self.opts.val_txt_name_list)
+        self.val_loader = DataLoader(self.val_dataset, batch_size=self.opts.batch_size, shuffle=False)
 
         self.train_nonPed_num, self.train_ped_num = self.train_dataset.get_ped_cls_num()
         self.val_nonPed_num, self.val_ped_num = self.val_dataset.get_ped_cls_num()

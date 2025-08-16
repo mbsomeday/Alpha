4dd9c2c92cb7cd79a1a8274d7714428a32201648
mbsomeday <137542994@qq.com>
2025-08-16 14:26:54 +0100

    update

--- main_test.py
+++ main_test.py
@@ -1,48 +1,38 @@
-# 将上级目录加入 sys.path， 防止命令行运行时找不到包
-import os, sys
-curPath = os.path.abspath(os.path.dirname(__file__))
-root_path = os.path.split(curPath)[0]
-sys.path.append(root_path)
-
 import argparse
 
-from training.training_template import Ped_Classifier
-from configs.pedCls_args import BaseArgs, TrainArgs, TestArgs
+from data.dataset import my_dataset
+from training_template import DS_Classifier
 
 
-opts = TestArgs().parse()
-ped_cls = Ped_Classifier(opts=opts)
-ped_cls.test()
+def get_opts():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--ds_name_list', nargs='+', default=['D1', 'D2', 'D3'])
+    # parser.add_argument('--txt_name', type=str, default='test.txt')
+    parser.add_argument('--path_key', type=str, default='Stage6_org')
+    parser.add_argument('--ds_model_obj', type=str, default='models.EfficientNet.efficientNetB0')
+
+    parser.add_argument('--isTrain', action='store_true')
+
+    # test
+    parser.add_argument('--ds_weights_path', type=str, default=r'D:\my_phd\Model_Weights\Stage6\new_dataset\dsClsD1D2D3-08-1.09839.pth')
+    parser.add_argument('--test_batch_size', type=int, default=32)
+    parser.add_argument('--test_txt_name', type=str, default='test.txt')
+
+    opts = parser.parse_args()
+    return opts
 
 
-# from test_func.ds_cls import test_ds_classifier
-# def get_opts():
-#     parser = argparse.ArgumentParser()
-#     """Define the common options that are used in both training and test."""
-#     parser.add_argument('--weights_path', type=str)
-#     return parser.parse_args()
-#
-# opts = get_opts()
-# weights_path = opts.weights_path
-# ds_cls = test_ds_classifier(model_obj='models.EfficientNet.efficientNetB0',
-#                             ds_name_list=['D1', 'D2'],
-#                             batch_size=96,
-#                             weights_path=weights_path,
-#                             txt_name='test.txt',
-#                             path_key='Stage6_org'
-#                             )
+opts = get_opts()
+
+ds_classifier = DS_Classifier(opts)
+ds_classifier.test()
 
 
-# from test_func.ds_cls import test_ds_classifier
-#
-#
-# test_ds_classifier(model_obj='models.EfficientNet.efficientNetB0',
-#                    ds_name_list=['D1','D4'],
-#                    path_key='Stage6_org',
-#                    weights_path=r'/kaggle/input/stage5-weights-effidscls/efficientNetB0_dsCls-10-0.97636.pth',
-#                    batch_size=96,
-#                    txt_name='val.txt'
-#                    )
+
+
+
+
 
 
 

5c89fe7a885e3ec3be76a15117d7db14213626d8
mbsomeday <137542994@qq.com>
2025-08-25 16:56:20 +0100

    update

--- main_test.py
+++ main_test.py
@@ -1,22 +1,27 @@
 import argparse
 
-from training.training_template import DS_Classifier
+# from training.training_template import DS_Classifier
+from test_func.ds_cls import test_ds_classifier
 
 
 def get_opts():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--ds_name_list', nargs='+', default=['D1', 'D2', 'D3'])
-    # parser.add_argument('--txt_name', type=str, default='test.txt')
     parser.add_argument('--path_key', type=str, default='Stage6_org')
     parser.add_argument('--ds_model_obj', type=str, default='models.EfficientNet.efficientNetB0')
 
     parser.add_argument('--isTrain', action='store_true')
 
     # test
-    parser.add_argument('--ds_weights_path', type=str, default=r'D:\my_phd\Model_Weights\Stage6\new_dataset\dsClsD1D2D3-08-1.09839.pth')
+    parser.add_argument('--weights_path', type=str, default=r'D:\chrom_download\dsClsD1D2D3-03-16.15992.pth')
     parser.add_argument('--test_batch_size', type=int, default=32)
     parser.add_argument('--test_txt_name', type=str, default='test.txt')
+
+    # dataset
+    parser.add_argument('--dir_path', type=str, default=r'D:\my_phd\dataset\Stage6\black_masked\blackMask_D2')
+    parser.add_argument('--label', type=int, default=0)
+    parser.add_argument('--batch_size', type=int, default=32)
 
     opts = parser.parse_args()
     return opts
@@ -24,11 +29,16 @@ def get_opts():
 
 opts = get_opts()
 
-ds_classifier = DS_Classifier(opts)
-ds_classifier.test()
+# ds_classifier = DS_Classifier(opts)
+# ds_classifier.test()
 
-
-
+model_obj = opts.ds_model_obj
+ds_name_list = opts.ds_name_list
+path_key = opts.path_key
+weights_path = opts.weights_path
+batch_size = opts.batch_size
+txt_name = 'test.txt'
+test_ds_classifier(model_obj, ds_name_list, path_key, weights_path, batch_size, txt_name, opts=opts)
 
 
 
--- test_func/ds_cls.py
+++ test_func/ds_cls.py
@@ -4,11 +4,11 @@ from tqdm import tqdm
 from sklearn.metrics import confusion_matrix, balanced_accuracy_score
 
 
-from data.dataset import my_dataset
+from data.dataset import my_dataset, dataset_from_dir
 from utils.utils import load_model, get_obj_from_str, DEVICE
 
 
-def test_ds_classifier(model_obj, ds_name_list, path_key, weights_path, batch_size, txt_name):
+def test_ds_classifier(model_obj, ds_name_list, path_key, weights_path, batch_size, txt_name, opts=None):
     '''
         测试数据集分类模型
     :param model_obj:
@@ -16,12 +16,14 @@ def test_ds_classifier(model_obj, ds_name_list, path_key, weights_path, batch_si
     :param batch_size:
     :return:
     '''
-    ds_model = get_obj_from_str(model_obj)(num_class=4)
+    ds_model = get_obj_from_str(model_obj)(num_class=3)
     ds_model = load_model(ds_model, weights_path).to(DEVICE)
     ds_model.eval()
 
-    ds_dataset = my_dataset(ds_name_list=ds_name_list, path_key=path_key, txt_name=txt_name)
+    # ds_dataset = my_dataset(ds_name_list=ds_name_list, path_key=path_key, txt_name=txt_name)
+    ds_dataset = dataset_from_dir(opts.dir_path, opts.label)
     ds_loader = DataLoader(ds_dataset, batch_size=batch_size, shuffle=False)
+    print(len(ds_dataset))
 
     correct_num = 0
     y_pred = []
@@ -30,7 +32,8 @@ def test_ds_classifier(model_obj, ds_name_list, path_key, weights_path, batch_si
     with torch.no_grad():
         for idx, data_dict in enumerate(tqdm(ds_loader)):
             images = data_dict['image'].to(DEVICE)
-            ds_label = data_dict['ds_label'].to(DEVICE)
+            # ds_label = data_dict['ds_label'].to(DEVICE)
+            ds_label = data_dict['label'].to(DEVICE)
 
             logits = ds_model(images)
             probs = torch.argmax(logits, 1)
@@ -40,25 +43,38 @@ def test_ds_classifier(model_obj, ds_name_list, path_key, weights_path, batch_si
 
             correct_num += (ds_label == probs).sum()
 
-        cm = confusion_matrix(y_label, y_pred)
-        print(f'Testing cm:\n {cm}')
 
-        ds_accuracy = correct_num / len(ds_dataset)
-        print(f'准确率为：{ds_accuracy}')
+        # 分别计算0，1，2的数量
+        pred_as_D1 = y_pred.count(0)
+        pred_as_D2 = y_pred.count(1)
+        pred_as_D3 = y_pred.count(2)
 
-        bc = balanced_accuracy_score(y_label, y_pred)
-        print(f'balanced accuracy为：{bc}')
+        print(f'预测为来自D1:{pred_as_D1}, D2:{pred_as_D2}, D3:{pred_as_D3}')
+
+        # cm = confusion_matrix(y_label, y_pred)
+        # print(f'Testing cm:\n {cm}')
+        #
+        # ds_accuracy = correct_num / len(ds_dataset)
+        # print(f'准确率为：{ds_accuracy}')
+        #
+        # bc = balanced_accuracy_score(y_label, y_pred)
+        # print(f'balanced accuracy为：{bc}')
 
 
 
 
-#
-# if __name__ == '__main__':
-#     model_obj = 'models.EfficientNet.efficientNetB0'
-#     weights_path = r'C:\Users\wangj\Desktop\efficientB0\efficientB0_dsCls\efficientNetB0_dsCls-10-0.97636.pth'
-#     # weights_path = r'/data/jcampos/jiawei_data/code/efficientNetB0_dsCls/efficientNetB0_dsCls-10-0.97636.pth'
-#
-#     tt = test_ds_classifier(model_obj=model_obj, weights_path=weights_path, batch_size=8)
+
+if __name__ == '__main__':
+    print('Start')
+    # model_obj = 'models.EfficientNet.efficientNetB0'
+    # weights_path = r'D:\my_phd\Model_Weights\Stage5\EfficientNetB0_Scratch\efficientNetB0_dsCls-10-0.97636.pth'
+    #
+    # tt = test_ds_classifier(model_obj=model_obj,
+    #                         weights_path=weights_path,
+    #                         batch_size=8, txt_name='test.txt', path_key='Stage6_org', ds_name_list=['D4'])
+
+    get_dtype = torch.get_default_dtype()
+    print(get_dtype)
 
 
 
